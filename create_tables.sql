6dd5ac4e456fd3ff252bac53df7a4b4c1b7027bf
angelamende-gel <angela.mendez.cvt@eac.edu.ph>
2026-09-15 04:13:26 -0700

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -1,33 +1,32 @@
-CREATE TABLE Customers(
+CREATE TABLE Customers (
     CustomerID INT AUTO_INCREMENT PRIMARY KEY,
     Name VARCHAR(100) NOT NULL,
     Phone VARCHAR(20),
     Email VARCHAR(100)
 );
 
-
-CREATE TABLE Employees(
+CREATE TABLE Employees (
     EmployeeID INT AUTO_INCREMENT PRIMARY KEY,
     Name VARCHAR(100) NOT NULL,
     Position VARCHAR(50)
-    );
+);
 
-CREATE TABLE Flower(
+CREATE TABLE Flowers (
     FlowerID INT AUTO_INCREMENT PRIMARY KEY,
     FlowerName VARCHAR(100) NOT NULL,
     Category VARCHAR(50),
     Price DECIMAL(10,2) NOT NULL
-    );
+);
 
 CREATE TABLE Orders (
     OrderID INT AUTO_INCREMENT PRIMARY KEY,
     CustomerID INT NOT NULL,
-    EmployeeID NOT NULL,
-    OrderDateID NOT NULL,
+    EmployeeID INT NOT NULL,
+    OrderDate DATE NOT NULL,
     TotalAmount DECIMAL(10,2) NOT NULL,
     FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID),
     FOREIGN KEY (EmployeeID) REFERENCES Employees(EmployeeID)
-    );
+);
 
 CREATE TABLE OrderDetails (
     OrderDetailID INT AUTO_INCREMENT PRIMARY KEY,
@@ -36,10 +35,9 @@ CREATE TABLE OrderDetails (
     Quantity INT NOT NULL,
     Subtotal DECIMAL(10,2) NOT NULL,
     FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
-    FOREIGN KEY (FlowerID) REFERENCES Flowers (FlowerID)
-    );
+    FOREIGN KEY (FlowerID) REFERENCES Flowers(FlowerID)
+);
 
-    
 CREATE TABLE Payments (
     PaymentID INT AUTO_INCREMENT PRIMARY KEY,
     OrderID INT NOT NULL UNIQUE,
@@ -47,5 +45,4 @@ CREATE TABLE Payments (
     PaymentMethod VARCHAR(30),
     Amount DECIMAL(10,2) NOT NULL,
     FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
-    );
-    
+);

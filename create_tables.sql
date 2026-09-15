19b5404f49dd5903e34e38b74c742151207868d6
angelamende-gel <angela.mendez.cvt@eac.edu.ph>
2026-09-15 02:46:36 -0700

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -1,6 +1,51 @@
-CREATE TABLE Customers (
+CREATE TABLE Customers(
     CustomerID INT AUTO_INCREMENT PRIMARY KEY,
     Name VARCHAR(100) NOT NULL,
     Phone VARCHAR(20),
-    Email VARCHAR(100)
+    Email VARCHAR(100),
 );
+
+
+CREATE TABLE Employees(
+    EmployeeID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(100) NOT NULL,
+    Position VARCHAR(50),
+    );
+
+CREATE TABLE Flower(
+    FlowerID INT AUTO_INCREMENT PRIMARY KEY,
+    FlowerName VARCHAR(100) NOT NULL,
+    Category VARCHAR(50),
+    Price DECIMAL(10,2) NOT NULL,
+    );
+
+CREATE TABLE Orders (
+    OrderID INT AUTO_INCREMENT PRIMARY KEY,
+    CustomerID INT NOT NULL,
+    EmployeeID NOT NULL,
+    OrderDateID NOT NULL,
+    TotalAmount DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID),
+    FOREIGN KEY (EmployeeID) REFERENCES Employees(EmployeeID),
+    );
+
+CREATE TABLE OrderDetails (
+    OrderDetailID INT AUTO_INCREMENT PRIMARY KEY,
+    OrderID INT NOT NULL,
+    FlowerID INT NOT NULL,
+    Quantity INT NOT NULL,
+    Subtotal DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
+    FOREIGN KEY (FlowerID) REFERENCES Flowers (FlowerID),
+    );
+
+    
+CREATE TABLE Payments (
+    PaymentID INT AUTO_INCREMENT PRIMARY KEY,
+    OrderID INT NOT NULL UNIQUE,
+    PaymentDate DATE NOT NULL,
+    PaymentMethod VARCHAR(30),
+    Amount DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
+    );
+    
